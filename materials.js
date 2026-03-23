22e5120c32336a8e4a52f5230822aa0b3e384364
Jashandeep Singh Sidhu <76151136+jashansingh9888@users.noreply.github.com>
2026-03-23 13:46:25 -0400

    Create materials.js

---
+++ materials.js
@@ -0,0 +1,52 @@
+const materialCatalog = [
+    // COPPER
+    { name: "RJ45 Connector", sap: "1000289", category: "Copper" },
+    { name: "2 Port Faceplate", sap: "1001047", category: "Copper" },
+    { name: "4 Port Faceplate", sap: "1001048", category: "Copper" },
+    { name: "White Jacks", sap: "1000520", category: "Copper" },
+    { name: "Coax Insert", sap: "1001049", category: "Copper" },
+    { name: "3ft CAT5 Patch Cable", sap: "1000254", category: "Copper" },
+    { name: "5ft CAT5 Patch Cable", sap: "1000497", category: "Copper" },
+    { name: "10ft CAT5 Patch Cable", sap: "1000253", category: "Copper" },
+
+    // FIBRE
+    { name: "Fibre Jack", sap: "1002322", category: "Fibre" },
+    { name: "Heat Sleeve 40mm", sap: "1000526", category: "Fibre" },
+    { name: "LC APC Connector", sap: "1001083", category: "Fibre" },
+    { name: "SC APC SC APC Patches", sap: "1002388", category: "Fibre" },
+    { name: "LC APC LC APC Patches", sap: "1002335", category: "Fibre" },
+    { name: "Heat Sleeve 25mm", sap: "1002592", category: "Fibre" },
+    { name: "Heat Sleeve 50mm", sap: "1002593", category: "Fibre" },
+    { name: "APC LC APC SC Patches", sap: "1002597", category: "Fibre" },
+    { name: "SC APC Connector", sap: "1001084", category: "Fibre" },
+    { name: "LC UPC 250/900", sap: "1000422", category: "Fibre" },
+    { name: "SC UPC", sap: "1000476", category: "Fibre" },
+    { name: "APC Bulk Heads", sap: "1001079", category: "Fibre" },
+    { name: "UPC Bulk Heads", sap: "1000461", category: "Fibre" },
+
+    // HARDWARE
+    { name: "5222XG Zhone", sap: "1002324", category: "Hardware" },
+    { name: "2424GN Zhone", sap: "1002327", category: "Hardware" },
+    { name: "2424A1 Zhone", sap: "1001199", category: "Hardware" },
+    { name: "2424A Zhone", sap: "100446", category: "Hardware" },
+    { name: "2628A Zhone", sap: "1001087", category: "Hardware" },
+    { name: "8 port GPON zhone", sap: "1001204", category: "Hardware" },
+    { name: "Nokia Beacon 19", sap: "1002555", category: "Hardware" },
+    { name: "Nokia XS-010X-R", sap: "1002557", category: "Hardware" },
+    { name: "Airties 4960x", sap: "1002344", category: "Hardware" },
+    { name: "Airties 4960", sap: "1002207", category: "Hardware" },
+    { name: "Archer C20 (100MBS)", sap: "1002407", category: "Hardware" },
+    { name: "TP LINK ARCHER AX23", sap: "1002481", category: "Hardware" },
+    { name: "SML TV Box", sap: "1001554", category: "Hardware" },
+
+    // OTHER
+    { name: "A1 Zhone Mount", sap: "1000420", category: "Other" },
+    { name: "XGS / 2424GN Zhone Mount", sap: "1002445", category: "Other" },
+    { name: "Nokia ONT Mount - Factory", sap: "1002573", category: "Other" },
+    { name: "Nokia VOIP ONT mount - Factory", sap: "1002572", category: "Other" },
+    { name: "Nokia ONT Mount - Custom", sap: "1002589", category: "Other" },
+    { name: "Nokia VOIP ONT mount - Custom", sap: "1002590", category: "Other" },
+    { name: "RJ11 Splitter (Dongle)", sap: "1000501", category: "Other" },
+    { name: "HDMI Cable 6'", sap: "1000952", category: "Other" },
+    { name: "Power Extension", sap: "1001208", category: "Other" }
+];
